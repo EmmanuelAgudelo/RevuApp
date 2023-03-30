3ebac8478a2b7fcbe1b92bbfea3735b3ae6bdac8
Diego Yepes <Diegoyepes520@gmail.com>
2023-03-30 14:18:31 -0500

    Form register

--- src/components/index.ts
+++ src/components/index.ts
@@ -1,7 +1,11 @@
 export * from "./layout/Layout";
 export * from './home/FrontPage';
 
+
+/* Auth */
 export * from './auth/LoginComponen';
+export * from './auth/RegisterComponent';
+
 
 /* Download */
 export * from "./download/Download";
--- src/pages/auth/Register.tsx
+++ src/pages/auth/Register.tsx
@@ -1,6 +1,8 @@
 import { useEffect } from "react";
 import { setTitle } from "../../helpers";
 
+import RegisterComponent from "../../components/auth/RegisterComponent";0
+
 const Register = () => {
 
   useEffect(() => {
@@ -8,7 +10,9 @@ const Register = () => {
   }, [])
   
   return (
-    <div>Register</div>
+<>  
+<RegisterComponent/>
+</>
   )
 }
 
